acf2c626c404bd26a1e0eaff00cd28ee8d4ea79c
Taj <tajmarie92@gmail.com>
2022-06-14 20:53:00 -0500

    Add project files.

---
+++ ListsCsharp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ListsCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] food = new string[10];
+
+            food[0] = "pizza";
+            food[1] = "sandwich";
+            food[2] = "pasta";
+            food[3] = "tacos";
+            food[4] = "steak";
+            food[5] = "pork chops";
+            food[6] = "crab legs";
+            food[7] = "salmon";
+            food[8] = "cheeseburger";
+            food[9] = "green beans";
+
+            foreach (String item in food)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
